0faafc9de85ff107847a81d319ffc782bdbaf664
jerry <jeong20909@naver.com>
2023-02-21 13:43:45 +0900

    [fix] ex01 내용 수정

--- prac/example01.js
+++ prac/example01.js
@@ -12,17 +12,3 @@ let c = 2312.213
 const d = "test2";
 
 
-
-const mulWithArrowFunc = (x, y) => {
-    return x * y;
-};
-
-
-const divWithArrowFunc = (x, y) => {
-    return x / y;
-};
-
-
-
-console.log(mulWithArrowFunc(2, 3));
-console.log(divWithArrowFunc(6, 4));
