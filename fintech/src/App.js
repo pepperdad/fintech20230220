727aff39c6fe74e91d2462b9a8569ec1eaff0e83
jerry <jeong20909@naver.com>
2023-02-21 14:26:17 +0900

    [feat] 컴포넌트에 Props 전달 - Components와 props

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,22 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const Welcome = (props) => {
+  console.log(props)
+  return <p>안녕하세요. {props.userAge}세 {props.userHeight}cm {props.userName} 님!</p>;
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome userName="정도영" userAge={35} userHeight={175}></Welcome>
+      <Welcome userName="유관우" userAge={35} userHeight={175}></Welcome>
+      <Welcome userName="홍길동" userAge={35} userHeight={175}></Welcome>
     </div>
   );
 }
