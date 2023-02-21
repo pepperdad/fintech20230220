df91a1c40a52f500096449d4f77f97f4b7db1adf
jerry <jeong20909@naver.com>
2023-02-21 15:57:24 +0900

    [feat] Styled-Component module 추가 설치 - 설치 명령어 : npm install styled-components

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import InputComponents from "./components/InputComponents";
+import ListComponentes from "./components/ListComponents";
+import StyledComponent from "./components/StyledComponents";
 import Welcome from "./components/Welcome"
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Welcome userName="정도영" userAge={35} userHeight={175}></Welcome>
 
       <InputComponents></InputComponents>
+      <ListComponentes></ListComponentes>
+      <StyledComponent></StyledComponent>
     </div>
   );
 }
--- fintech/src/components/InputComponents.js
+++ fintech/src/components/InputComponents.js
@@ -4,7 +4,7 @@ const InputComponents = (props) => {
     // let value = "test";
     const [name, setName] = useState("test"); // 1번째 인자를 두번째 setName으로 바꾼다고 생각!
     const handleChangeInput = (event) => {
-        setName(event.target.value)
+        setName(event.target.value);
     };
 
     const handleClickButton = () => {
