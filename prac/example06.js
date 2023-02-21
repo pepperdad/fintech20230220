01889469ab180d48bbf945e7185b2c1f879f0775
jerry <jeong20909@naver.com>
2023-02-21 13:12:44 +0900

    [feat] 템플릿 리터럴 문법 추가

---
+++ prac/example06.js
@@ -0,0 +1,12 @@
+let car = {
+    name: "sonata",
+    ph: 160,
+    year: 2022,
+};
+
+const { name, ph, year } = car;
+
+console.log(name + "는 " + ph + " 마력 " + year + " 년식 입니다.");
+
+// `` <- 백틱 사용
+console.log(`${name}는 ${ph} 마력 ${year} 년식 입니다.`);
