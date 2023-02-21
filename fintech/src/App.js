859879dbdae8b45667b8d29f5ed401da979b41db
jerry <jeong20909@naver.com>
2023-02-21 16:35:01 +0900

    [feat] Axios 실습 페이지 추가 - Axios 라이브러리 실습 페이지 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InputComponents from "./components/InputComponents";
 import ListComponentes from "./components/ListComponents";
+import AxiosComponents from "./components/AxiosComponent";
 import StyledComponent from "./components/StyledComponents";
 import Welcome from "./components/Welcome"
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Welcome />}></Route>
         <Route path="/input" element={<InputComponents />}></Route>
         <Route path="/list" element={<ListComponentes />}></Route>
+        <Route path="/axios" element={<AxiosComponents />}></Route>
       </Routes>
     </BrowserRouter>
   );
