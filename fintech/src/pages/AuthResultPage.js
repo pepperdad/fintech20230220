7236b6864361829e9e08e1c1c57d627dbfe87c91
jerry <jeong20909@naver.com>
2023-02-22 16:21:56 +0900

    [feat] Axios를 통한 AccessToken API 요청 작성 - SendData 선언부 확인 - option 선언부 확인 ** client 정보 변경 필수

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -2,16 +2,43 @@ import React from 'react'
 import { useLocation } from 'react-router-dom';
 import AppHeader from '../components/Common/AppHeader';
 import queryString from "query-string"
+import axios from 'axios';
 
 const AuthResultPage = () => {
     const code = useLocation().search;
     const authCode = queryString.parse(code).code;
 
+    const handleAuthButtonClick = () => {
+        let sendData = {
+            code: authCode,
+            client_id: "5ffdfea1-7d7f-4b4f-9d69-611c65eee6d0", // 본인 클라이언트 정보를 활용해주세요
+            client_secret: "5810325a-6f56-4d2f-9b12-4456a5a40161", // 본인 클라이언트 정보를 활용해주세요
+            redirect_uri: "http://localhost:3000/authResult",
+            grant_type: "authorization_code"
+        };
+
+        const option = {
+            method: "POST",
+            url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            data: sendData,
+        };
+
+        axios(option).then((response) => {
+            console.log(response);
+        })
+
+    }
+
     return (
         <div>
             <AppHeader title={"인증완료 / 토큰 요청"}></AppHeader>
-            사용자 authCode : {authCode}
+            사용자 authCode : {authCode} <br></br>
+            <button onClick={handleAuthButtonClick}>AccessToken 요청</button>
         </div>
+
     );
 };
 
