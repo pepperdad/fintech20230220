327af301c938dc502ea810b5ed2ab633ede74519
jerry <jeong20909@naver.com>
2023-02-24 11:01:39 +0900

    [work] work07 출금 이체 기능 구현 1/2 - 출금 이체 발생시키기 data params json tran_amt, fintech_use_num, req_client_fintech_use_num, bank_tran_id 고정값 사용 금지 나머지는 고정값으로 axios option으로 요청을 작성하기 <- api 요청 application/json 은 데이터를 어떻게 전송? 결과를 로그로 작성

--- fintech/src/components/Withdraw/ModalCard.js
+++ fintech/src/components/Withdraw/ModalCard.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import queryString from "query-string";
 import React, { useState } from "react";
 import styled from "styled-components";
 
@@ -33,16 +34,64 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "M202201564U" + countnum; //이용기과번호 본인것 입력
+    let transId = "M202300416U" + countnum; //이용기과번호 본인것 입력
     return transId;
   };
 
-  const handlePayButtonClick = () => {};
+  const handlePayButtonClick = () => {
+    // 출금 이체 발생시키기
+    // data params json
+    // tran_amt, fintech_use_num, req_client_fintech_use_num, bank_tran_id 고정값 사용 금지 나머지는 고정값으로
+    // axios option으로 요청을 작성하기 <- api 요청
+    // application/json 은 데이터를 어떻게 전송?
+    // 결과를 로그로 작성
 
-  const deposit = () => {};
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+
+    let data = {
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "사용료",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "사용료",
+      tran_amt: amount,
+      tran_dtime: "20230223160900",
+      req_client_name: "이용료(홍길동)",
+      req_client_fintech_use_num: tofintechno,
+      req_client_num: "DOYONQ",
+      transfer_purpose: "ST",
+      recv_client_name: "정도영",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "300000000001"
+    }
+
+    // const parsedSendData = queryString.stringify(data); 필요없음?
+
+    const option = {
+
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+    });
+
+  };
+
+  const deposit = () => {
+
+  };
 
   const handleChange = (e) => {
     const { value } = e.target;
+    console.log(value);
     setamount(value);
   };
 
--- fintech/src/components/Withdraw/ModalWithdraw.js
+++ fintech/src/components/Withdraw/ModalWithdraw.js
@@ -23,18 +23,39 @@ const ModalWithdraw = ({ tofintechno }) => {
     slidesToScroll: 1,
   };
 
-  const [acountList, setAccountList] = useState([]);
+  const [accountList, setAccountList] = useState([]);
 
   useEffect(() => {
     getAccountList();
   }, []);
 
-  const getAccountList = () => { };
+  const getAccountList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const userSeqNo = localStorage.getItem("userSeqNo");
+    console.log(accessToken, userSeqNo);
+    //axios 요청을 작성해야함
+    //header 설정
+    const option = {
+      method: "GET",
+      url: "/v2.0/user/me",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        user_seq_no: userSeqNo,
+      },
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+      setAccountList(data.res_list);
+    });
+  };
 
   return (
     <ModalWithdrawBlock>
       <Slider {...settings}>
-        {acountList.map((account) => {
+        {accountList.map((account) => {
           return (
             <ModalCard
               key={account.fintech_use_num}
