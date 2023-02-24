4c0d85c8b75040c0fc0c0eb60339a744a0da6bc8
jerry <jeong20909@naver.com>
2023-02-24 15:03:23 +0900

    [feat] nodejs 동기 비동기 (1/2) - 비동기 코드 예제 파일 읽어오기

--- fintech_server/expressServer.js
+++ fintech_server/expressServer.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express(); // respond with "hello world" when a GET request is made to the homepage
 
 app.get("/", (req, res) => {
+    console.log("사용자의 요청이 발생했습니다.");
     res.send("hello world");
 });
 
