ac6a7a2ac39f33955c0dc321b1a0a15874504696
jerry <jeong20909@naver.com>
2023-02-24 15:10:54 +0900

    [feat] async / sync function 차이 - node async.js - node sync.js

--- fintech_server/prac/async.js
+++ fintech_server/prac/async.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 console.log("first func");
-fs.readFile("./test.txt", "utf8", function (err, result) {
+fs.readFile("./text.txt", "utf8", function (err, result) {
     if (err) {
         console.error(err);
         throw err;
